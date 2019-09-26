b14fcc68f6c86501f5ccd1526f6b249c3635f3b7
Abhisheikh Gill <abhisheikhg@gmail.com>
2019-09-26 19:03:30 -0400

    Add removeItem reducer/action/type and add removeItem to checkout-item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -21,6 +21,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.REMOVE_ITEM:
       return {
         ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
         cartItems: state.cartItems.filter(
           cartItem => cartItem.id !== action.payload.id
         )
