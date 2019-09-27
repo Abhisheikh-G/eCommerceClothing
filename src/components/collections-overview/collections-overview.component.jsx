9f24a15b7ea8791ef888fb87b491936519424314
Abhisheikh Gill <abhisheikhg@gmail.com>
2019-09-27 16:42:30 -0400

    Turned shop_data from array to object, added new shopSelector for collection-preview component.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selector";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 import "./collections-overview.styles.scss";
 
-const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
-    {collections.map(({ id, ...otherProps }) => (
-      <CollectionPreview key={id} {...otherProps} />
-    ))}
-  </div>
-);
+const CollectionsOverview = ({ collections }) => {
+  console.log(collections);
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherProps }) => (
+        <CollectionPreview key={id} {...otherProps} />
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
