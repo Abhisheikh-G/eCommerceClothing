ecfc7833b72d4e351e651bf05cf526e6df9ce2cf
Abhisheikh Gill <abhisheikhg@gmail.com>
2019-09-30 14:39:25 -0400

    Add stripe integration, update favicon and website title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_WcPBcY3Nnwmeha09VRKkn8EM00EWponIGR";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      currency="USD"
+      name="CRWN Clothing Ltd."
+      shippingAddress
+      billingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
